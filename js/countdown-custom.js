98d838f55a9097edb1f3bcbef4627c6f0ddef27d
Jorge Valdes <jc890825@gmail.com>
2024-04-18 21:36:10 +0200

    Changes Biachi 2

--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2024, 03, 01, 8)}); // year, month, date, hour
-        });
-});		
-
