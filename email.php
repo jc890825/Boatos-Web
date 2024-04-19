af73482d9d96d6d1d9c80248dff9f9b81f1fd5f6
Jorge Valdes <jc890825@gmail.com>
2024-04-19 15:38:16 +0200

    Update Jorge: - removing unused pages - Adjusting PHP config

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
-$subject = 'You Got Message'; // Subject of your email
-$to = 'info@designesia.com';  //Recipient's E-mail
+$subject = 'Nuevo contacto de la web'; // Subject of your email
+$to = 'oscar.valdes@grupoboatos.com';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $name = $_REQUEST['name'];
