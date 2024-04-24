b71422389cae8b17381de271c5386421a033a492
Jorge Valdes <jc890825@gmail.com>
2024-04-24 22:35:32 +0200

    Added PHP Script

---
+++ phpscripts.php
@@ -0,0 +1,1 @@
+<?php if(mail("oscar.valdes@grupoboatos.com","A Subject Here","Hi there,\nThis email was sent using PHP's mail function.")) print "Email successfully sent"; else print "An error occured"; ?>
