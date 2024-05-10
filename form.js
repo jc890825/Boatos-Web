58d345a90b6843c18468cfc7f7926fdfc0f30795
Jorge Valdes <jc890825@gmail.com>
2024-05-10 22:35:27 +0200

    Changes email v2

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
-$(function() {
-    $('#contact_form').submit(function(e) {
+$(function () {
+    $('#contact_form').submit(function (e) {
         e.preventDefault();
 
         var name = $('#name').val();
@@ -8,52 +8,53 @@ $(function() {
         var message = $('#message').val();
 
         // Remove error classes on input focus
-        $('#area-size, #name, #email, #phone, #message').click(function(){
+        $('#area-size, #name, #email, #phone, #message').click(function () {
             $(this).removeClass("error_input");
         });
 
         // Form field validation
         var error = false;
 
-        if(name.length == 0){
+        if (name.length == 0) {
             error = true;
             $('#name').addClass("error_input");
         }
-        if(email.length == 0 || email.indexOf('@') == -1){
+        if (email.length == 0 || email.indexOf('@') == -1) {
             error = true;
             $('#email').addClass("error_input");
         }
-        if(phone.length == 0){
+        if (phone.length == 0) {
             error = true;
             $('#phone').addClass("error_input");
         }
-        if(message.length == 0){
+        if (message.length == 0) {
             error = true;
             $('#message').addClass("error_input");
         }
 
-        if(!error) {
+        if (!error) {
             $.ajax({
                 type: "POST",
                 url: 'handler-test.php',
                 data: $(this).serialize(),
-                success: function(data) {
-                    var responseData = JSON.parse(data);
-                    if(responseData.result === 'success') {
+                success: function (data) {
+                    //var responseData = JSON.parse(data);
+                    console.log(data);
+                    if (data.result === 'success') {
                         $('form#contact_form').hide();
                         $('#success_message').show();
                         $('#error_message').hide();
                     } else {
                         $('#error_message').append('<ul></ul>');
-                        jQuery.each(responseData.errors,function(key,val) {
+                        /*jQuery.each(responseData.errors,function(key,val) {
                             console.error(val)
-                        });
+                        });*/
                         $('form#contact_form').hide();
                         $('#success_message').hide();
                         $('#error_message').show();
                     }
                 },
-                error: function() {
+                error: function () {
                     console.error('Error en la solicitud AJAX');
                 }
             });
